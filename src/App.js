5ad2b177ab2dfb6deab2dd48b1446c1d416d76fd
Haoran Zhou <kens0serious@gmail.com>
2022-01-23 20:41:58 -0500

    completed general layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from 'react'
+import Menu from './Menu'
+import Categories from './Categories'
+import items from './data'
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const list = items.map(({ category }) => category)
+
+  // remove duplicates
+  const newCategories = list.reduce(
+    (total, curr) => {
+      if (!total.includes(curr)) {
+        total.push(curr)
+      }
+      return total
+    },
+    ['all']
+  )
+
+  const [categories, setcategories] = useState(newCategories)
+  const [menuItems, setMenuItems] = useState(items)
+
+  return (
+    <main className="menu section">
+      <div className="title">
+        <h2>our menu</h2>
+        <div className="underline"></div>
+      </div>
+      <Categories categories={categories}></Categories>
+      <Menu items={menuItems}></Menu>
+    </main>
+  )
 }
 
-export default App;
+export default App
--- src/Categories.js
+++ src/Categories.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React from 'react'
 
-const Categories = () => {
-  return <h2>categories component</h2>;
-};
+const Categories = ({ categories }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button key={index} className="filter-btn">
+            {category}
+          </button>
+        )
+      })}
+    </div>
+  )
+}
 
-export default Categories;
+export default Categories
